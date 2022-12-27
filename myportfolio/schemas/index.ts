12c93fbc21b15c26624f9508baa9241b67a507ed
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2022-12-27 19:55:31 +0800

    remove sanity

--- myportfolio/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import experiences from './experiences'
-import pageInfo from './pageInfo'
-import projects from './projects'
-import skills from './skills'
-import social from './social'
-
-export const schemaTypes = [experiences, pageInfo, projects, skills, social]
